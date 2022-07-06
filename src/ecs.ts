485a04e9a5fa1f8e6a84c60a9733386e88d3ff3f
Miles Possing <milespossing@microsoft.com>
2022-07-05 17:30:55 -0700

    Updates

--- src/ecs.ts
+++ src/ecs.ts
@@ -1,135 +1,172 @@
-import { v4 } from 'uuid';
-import * as R from 'ramda';
+import { v4 } from "uuid";
+import * as R from "ramda";
 
-type Entity = string;
+export type Entity = string;
 
-interface IComponent {
-    entityId: number;
-    type: string;
-};
+// might want some way to deal with these a bit easier
+export interface IComponent {
+  entityId: Entity;
+  type: string;
+}
+
+export abstract class Component implements IComponent {
+  entityId!: Entity;
+  abstract type: string;
+}
 
 type ComponentContainer = Record<string, Record<Entity, IComponent>>;
 
 export interface ISystem {
-    name: string;
-    execute: (world: IWorld) => Promise<SystemResult>;
-};
+  name: string;
+  execute: (world: IWorld) => Promise<SystemResult>;
+}
 
 export class ComponentSystem implements ISystem {
-    name;
-    getFunc;
-    updateFunc;
-    
-    constructor(name: string, getComponents: (components: ComponentContainer) => IComponent[][], update: (components: IComponent[]) => Promise<void>){
-        this.name = name;
-        this.getFunc = getComponents;
-        this.updateFunc = update;
-    }
+  name;
+  getFunc;
+  updateFunc;
 
-    execute: (world: IWorld) => Promise<SystemResult> = async (world) => {
-        const components = this.getFunc(world.Components);
-        await Promise.all(components.map(this.updateFunc));
-        return new SystemResult();
-    };
+  constructor(
+    name: string,
+    getComponents: (components: ComponentContainer) => IComponent[][],
+    update: (components: IComponent[]) => Promise<void>
+  ) {
+    this.name = name;
+    this.getFunc = getComponents;
+    this.updateFunc = update;
+  }
+
+  execute: (world: IWorld) => Promise<SystemResult> = async (world) => {
+    const components = this.getFunc(world.Components);
+    await Promise.all(components.map(this.updateFunc));
+    return new SystemResult();
+  };
 }
 
 export class VoidSystem implements ISystem {
-    name: string;
-    func: any;
-    constructor(name: string, func: () => Promise<void>) {
-        this.name = name;
-        this.func = func;
-    }
-    
-    execute: (world: IWorld) => Promise<SystemResult> = async () => {
-        await this.func();
-        return new SystemResult();
-    };
+  name: string;
+  func: any;
+  constructor(name: string, func: (container: WorldContainer) => Promise<void>) {
+    this.name = name;
+    this.func = func;
+  }
+
+  execute: (world: IWorld) => Promise<SystemResult> = async () => {
+    await this.func();
+    return new SystemResult();
+  };
 }
 
 export interface IWorld {
-    Components: ComponentContainer;
-    Entities: Entity[];
-    Systems: ISystem[];
+  Components: ComponentContainer;
+  Entities: Entity[];
+  Systems: ISystem[];
 }
 
+export class WorldContainer {
+  World: IWorld;
+
+  constructor(world: IWorld) {
+    this.World = world;
+  }
+
+  addEntity: EntityBuilderFunc<void> = (func) => {
+    const newEntity = v4();
+    const builder = new EntityBuilder(newEntity);
+    func(builder);
+    this.World.Entities.push(newEntity);
+    builder.components.forEach(c => this.addComponent(newEntity, c));
+  };
+
+  addComponent: (entity: Entity, component: IComponent) => void = (
+    entity,
+    component
+  ) => {
+    component.entityId = entity;
+    this.World.Entities.push(entity);
+    if (!this.World.Components[component.type])
+      this.World.Components[component.type] = {};
+    this.World.Components[component.type][entity] = component;
+  };
+}
+
+type EntityBuilderFunc<T> = (
+  builderFunc: (builder: IEntityBuilder) => void
+) => T;
+
 interface IEntityBuilder {
-    WithComponent: (component: IComponent) => IEntityBuilder;
+  WithComponent: (component: IComponent) => IEntityBuilder;
 }
 
 class EntityBuilder implements IEntityBuilder {
-    entity: Entity;
-    components: IComponent[];
-    
-    constructor(entity: Entity) {
-        this.entity = entity;
-        this.components = [];
-    }
+  entity: Entity;
+  components: IComponent[];
 
-    WithComponent: (component: IComponent) => IEntityBuilder = (component) => {
-        this.components.push(component);
-        return this;
-    }
+  constructor(entity: Entity) {
+    this.entity = entity;
+    this.components = [];
+  }
+
+  WithComponent: (component: IComponent) => IEntityBuilder = (component) => {
+    component.entityId = this.entity;
+    this.components.push(component);
+    return this;
+  };
 }
 
 export class WorldBuilder implements IWorldBuilder {
-    Components: ComponentContainer;
-    Systems: ISystem[];
-    Entities: Entity[];
+  container: WorldContainer;
 
-    constructor() {
-        this.Components = {};
-        this.Systems = [];
-        this.Entities = [];
-    }
+  constructor() {
+    this.container = new WorldContainer({
+      Components: {},
+      Systems: [],
+      Entities: [],
+    });
+  }
 
-    WithEntity: (func: (builder: IEntityBuilder) => void) => IWorldBuilder = func => {
-        const newEntity = v4();
-        const builder = new EntityBuilder(newEntity);
-        func(builder);
+  WithEntity: EntityBuilderFunc<IWorldBuilder> = (func) => {
+    this.container.addEntity(func);
+    return this;
+  };
 
-        return this;
-    };
+  WithSystem: (system: ISystem) => IWorldBuilder = (system) => {
+    this.container.World.Systems.push(system);
+    return this;
+  };
 
-    WithSystem: (system: ISystem) => IWorldBuilder = (system) => {
-        this.Systems.push(system);
-        return this;
-    }
-
-    Build: () => IWorld = () => {
-        return {
-            Components: this.Components,
-            Systems: this.Systems,
-            Entities: this.Entities,
-        };
-    };
+  Build: () => WorldContainer = () => {
+    return this.container;
+  };
 }
 
 export interface IWorldBuilder {
-    Build: () => IWorld;
-    WithSystem: (system: ISystem) => IWorldBuilder;
-    WithEntity: (builderFunc: (builder: IEntityBuilder) => void) => IWorldBuilder;
+  Build: () => WorldContainer;
+  WithSystem: (system: ISystem) => IWorldBuilder;
+  WithEntity: EntityBuilderFunc<IWorldBuilder>;
 }
 
 export class SystemResult {
-    StdOut: string = "";
-    StdErr: string = "";
+  StdOut: string = "";
+  StdErr: string = "";
 }
 
 interface IStepResult {
-    SystemResults: SystemResult[];
+  SystemResults: SystemResult[];
 }
 
 interface IWorldRunner {
-    ExecuteStep: (world: IWorld) => Promise<IStepResult>;
+  ExecuteStep: (world: IWorld) => Promise<IStepResult>;
 }
 
 export class WorldRunner implements IWorldRunner {
-    ExecuteStep: (world: IWorld) => Promise<IStepResult> = async world => {
-        const results = await Promise.all(world.Systems.map(system => {
-            system.execute(world);
-        }));
-        R.flatten(results);
-        return { SystemResults: []}
-    };
+  ExecuteStep: (world: IWorld) => Promise<IStepResult> = async (world) => {
+    const results = await Promise.all(
+      world.Systems.map((system) => {
+        system.execute(world);
+      })
+    );
+    R.flatten(results);
+    return { SystemResults: [] };
+  };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
-import { ISystem, SystemResult, VoidSystem, WorldBuilder, WorldRunner } from "./ecs";
+import { Component, ISystem, SystemResult, VoidSystem, WorldBuilder, WorldRunner } from "./ecs";
+
+class TransactionIngested extends Component {
+    type: string = "TransactionIngested";
+    date: Date;
+
+    constructor(date: Date) {
+        super();
+        this.date = date;
+    }
+}
+
+class Price extends Component { 
+    type: string = "Price";
+    price: number = 0;
+}
+
+class Quantity extends Component {
+    type: string = "Quantity";
+    quantity: number = 0;
+}
+
 
 const helloSystem = new VoidSystem('hello world', async () => console.log('hello world'));
+
+const ingestorSystem = new VoidSystem('ingestor', async (world) => {
+    world.addEntity(builder => {
+        builder.WithComponent(new TransactionIngested(new Date())).WithComponent(new Price())
+    })
+
+
+});
 
 
 const main = async () => {
